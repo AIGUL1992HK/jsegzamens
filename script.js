cba40a3448a9655c70671194d871bbb79eeeebd0
Айгул Суйунева <ajgulsujunova@gmail.com>
2023-01-24 18:45:32 +0300

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello")
+const arr=["1","43","23","54","2", "3","89"]
+
+//первая задания
+const nevString=arr.join("")
+console.log(nevString)
+//4- задания
+console.log(arr.reverse())
+//6- задания
+const nevStr=arr.join(",")
+console.log(nevStr)
+
+for(let i = 0; i<arr.length;i++){
+    if(arr[i] % 2 ===0) {
+        const result=+arr[i]
+    }
+}
+console.log(arr)
+
